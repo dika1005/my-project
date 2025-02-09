c8e0a53b902e1b1a7d57c8395fed332488e497ac
dika ramadani <dikabangkok21@gmail.com>
2025-02-09 12:14:46 +0700

    Add Portofolio component with initial structure and image imports

---
+++ components/Portofolio.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React, { useState, useEffect } from "react"
+import Image from "next/image"
+import project1 from "@/public/assets/proj5.png"
+import project2 from "@/public/assets/proj6.png"
+import project3 from "@/public/assets/proj7.png"
+
+export const Portofolio = () => {
+    return(
+        <>
+        </>
+    )
+}
